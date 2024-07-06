7c9affdfda420194feb75ce86ac141f2a3110f26
junnun15 <junnunprodhan@gmail.com>
2024-07-06 23:45:11 +0600

    footer added

---
+++ src/routes/route.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+
+  } from "react-router-dom";
+
+import MainLayout from "../layout/MainLayout";
+import Home from "../components/Home/Home";
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        }
+      ]
+    },
+  ]);
